981248493cfb986166846c225858781544ed3ef2
Jolyn-Esther <jolineesther@gmail.com>
2025-04-07 12:47:58 +0300

    A function for adding elements in a circular queue

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -3,13 +3,7 @@
 #define size 5
 
 using namespace std;
-/*the modulus keeps the circular queue in sync(it adds to the front after dequing so that values can be continuosly
-added front the front since deletion is done at the front)
-in the dequeue function the front is the one modulusly added
-in the enqueue function the rear is the one that is modulusly added
-as long as the circular queue is not full, values will be added from the rear((rear= rear +1)% size ==front)
-as long as its not that the rare will be added and also after dequeing,
-a value can be added from the rear as long the modulus not equal to the front*/
+
 
 class CircularQueue
 {
@@ -50,84 +44,5 @@ CircularQueue()
         }
     }
     
-
-    void display()
-    {
-        cout << "The elements of the queue are as follows\n";
-        int i = front;
-        do
-        {
-            cout << arr_num[i] << " ";
-            cout << "\n";
-            i = (i + 1) % size;
-        } while (i != (rear + 1) % size);
-         /*as long as the front is not equal to the modulus of the rear, 
-        it will keep displaying the values*/
-    }
-
-    int dequeue()
-    { 
-        int x;
-        if (front == -1 && rear == -1) // checks if is empty
-        {
-            cout << "Queue is empty\n";
-            return 0;
-        }
-        else if (front == rear)
-        {                       
-            x = arr_num[front]; 
-            arr_num[front] = 0;
-            
-            front = -1;
-            rear = -1;
-            itemcount--;
-
-            return x;
-        }
-        else
-        {
-            x = arr_num[front];         
-            arr_num[front] = 0;       
-            front = (front + 1) % size; 
-            itemcount--;
-
-            return x;
-           
-        }
-    }
-
-    int count()
-    {
-        return (itemcount);
-       
-    }
-};
-
-int main()
-{
-    CircularQueue Q; // q is an object of the class
-    int value;
-
-    Q.enqueue(1);
-    Q.enqueue(3);
-    Q.enqueue(5);
-    Q.enqueue(6);
-    Q.enqueue(7);
-
-    value = Q.count();
-    cout << "The elements in the array are:" << value << "\n";
-
-    Q.dequeue();
-    Q.dequeue();
-
-    value = Q.count();
-    cout << "The elements in the array are:" << value << "\n";
-
-    Q.display();
-
-    Q.enqueue(12);
-    Q.enqueue(9);
-
-    Q.display();
-    return 0;
 }
+  
